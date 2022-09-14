14b580b77732e62df5410b8176ff248d2075d62e
Mikael Freva <mikaelfreva@live.fr>
2022-09-14 12:45:12 +0200

    list movie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import TopBar from "./Components/TopBar/TopBar";
+import Home from "./Components/Home/Home";
+
+export function makeRequest(specs) {
+  // return `https://api.themoviedb.org/3${specs}?api_key=4d91796accee221c06cb109a9805be7e`;
+
+   return `https://api.themoviedb.org/3${specs}?api_key=7fd7d7e0d432031c5ae55f16dced0d2a`;
+
+}
+
+export function makePosterImageUrl(posterPath) {
+  return `https://image.tmdb.org/t/p/original${posterPath}`;
+}
+
+export function makeBackdropImageUrl(url) {
+  return `https://image.tmdb.org/t/p/original${url}`;
+}
+
+export function classNames(...names) {
+  return names.filter((name) => name).join(" ");
+}
+
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar onSearchMovies={setSearchResults} />
+    
+      <Home searchResults={searchResults} />
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { classNames, makeRequest } from "../../App";
+import MoviePoster from "../MoviePoster/MoviePoster";
+import Style from "./Home.module.css";
+import MovieModal from "../MovieModal/MovieModal";
+import CastModal from "../CastModal/CastModal";
+
+export default function Home(props) {
+  const { searchResults } = props;
+  const url = makeRequest("/movie/popular");
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [showMovie, setShowMovie] = useState(false);
+  const [movie, setMovie] = useState({});
+  const [person, setPerson] = useState({});
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((movies) => setPopularMovies(movies.results));
+  }, [url]);
+
+  function onClickPoster(movie) {
+    setShowMovie(true);
+    setMovie(movie);
+  }
+
+  function onClickCastCard(person) {
+    setShowMovie(false);
+    setPerson(person);
+  }
+
+  function onClickMovie(movie) {
+    setPerson({});
+    setShowMovie(true);
+    setMovie(movie);
+  }
+
+  return (
+    <div className={classNames(Style.main, showMovie && Style.showMovie)}>
+      {searchResults.length > 0
+        ? searchResults.map((movie, index) => (
+            <MoviePoster
+              key={index}
+              movie={movie}
+              onClick={(movie) => onClickPoster(movie)}
+              hover={true}
+            />
+          ))
+        : popularMovies.map((movie, index) => (
+            <MoviePoster
+              key={index}
+              movie={movie}
+              onClick={onClickPoster}
+              hover={true}
+            />
+          ))}
+      {showMovie && (
+        <MovieModal
+          movie={movie}
+          onCloseModal={() => setShowMovie(false)}
+        />
+      )}
+ 
+    </div>
+  );
+}
